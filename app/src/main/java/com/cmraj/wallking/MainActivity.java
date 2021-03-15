694b627e66fdfbffefff586151e48de4abdf60be
CMRAJ2 <chandramohanraj23@gmail.com>
2021-03-15 14:47:10 +0530

    Application : wallKing Date & Time : 15 March 2021 & 14:46 Developers : Chandra Mohan, Christin B Koshy Version : 0.1v

--- app/src/main/java/com/cmraj/wallking/MainActivity.java
+++ app/src/main/java/com/cmraj/wallking/MainActivity.java
@@ -1,12 +1,18 @@
 package com.cmraj.wallking;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.AbsListView;
+import android.widget.EditText;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -34,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
     Boolean isScrolling = false;
     int currentItems,totalItems,scrollOutItems;
+    String url = "https://api.pexels.com/v1/curated/?page="+pageNumber+"&per_page=80";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void fetchWall(){
 
-        StringRequest request = new StringRequest(Request.Method.GET, "https://api.pexels.com/v1/curated/?page="+pageNumber+"&per_page=80",
+        StringRequest request = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
@@ -134,4 +141,36 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId()==R.id.navSearch){
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            final EditText editText = new EditText(this);
+            editText.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+
+            alert.setMessage("Enter Category e.g. Nature");
+            alert.setTitle("Search Wallpaper");
+            alert.setView(editText);
+
+            alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    String query = editText.getText().toString().toLowerCase();
+                    url = "https://api.pexels.com/v1/search/?page="+pageNumber+"&per_page=80&query="+query;
+                    wallModelList.clear();
+                    fetchWall();
+                }
+            })
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
